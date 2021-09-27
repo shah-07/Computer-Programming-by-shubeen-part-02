9a9350cd17516b36a4b533648838d1f0f19a104f
shahbaj00 <69644198+shahbaj00@users.noreply.github.com>
2021-09-27 14:07:57 +0600

    Update task01.c

--- Chapter One - Computer Memory/task01.c
+++ Chapter One - Computer Memory/task01.c
@@ -4,12 +4,15 @@ int main()
 {
     char word;
     word = getchar();
+    if(word <= 9 && word >= 0) {
+        word = word + 48;
+    }
 
-    if(word <= '9' && word >= '0') {
-        printf("0");
+    if(word <= 57 && word >= 48) {
+        printf("1");
     }
     else {
-        printf("1");
+        printf("0");
     }
 
 }
